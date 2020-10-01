9bec77ca1606046a7f121515ff6be0b0b01b60ea
Felipe-cdb <72224699+Felipe-cdb@users.noreply.github.com>
2020-10-01 17:03:02 -0300

    Update 1013.py

--- 1013.py
+++ 1013.py
@@ -9,8 +9,8 @@ b = int(b)
 c = int(c)
 d = int(d)
 
-maior1 = (a + b + abs(a - b)) / 2
+maior1 = ((a + b + abs(a - b)) / 2)
 
-maior2 = (maior1 + c + abs(maior1 - c)) / 2
+maior2 = ((maior1 + c + abs(maior1 - c)) / 2)
 
 print("{} eh o maior".format(int(maior2)))
