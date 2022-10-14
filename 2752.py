03b5e0c7886ea0909c1a214bbbc1d35a12ab065a
Danilo Correia <danilocorreiaviana@hotmail.com>
2022-10-14 16:07:20 -0300

    Create 2752.py

---
+++ 2752.py
@@ -0,0 +1,9 @@
+frase = "AMO FAZER EXERCICIO NO URI"
+print("<%s>" %frase)
+print("<%30s>" %frase)
+print("<%.20s>" %frase)
+print("<%-20s>" %frase)
+print("<%-30s>" %frase)
+print("<%.30s>" %frase)
+print("<%30.20s>" %frase)
+print("<%-30.20s>" %frase)
