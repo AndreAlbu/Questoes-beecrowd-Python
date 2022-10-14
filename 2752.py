23c68e3e96ef9a9b37e8ace7894e1bc24a8ae9f0
Danilo Correia <danilocorreiaviana@hotmail.com>
2022-10-14 14:49:25 -0300

    Delete 2752.py

--- 2752.py
+++
@@ -1,9 +0,0 @@
-frase = "AMO FAZER EXERCICIO NO URI"
-print("<%s>" %frase)
-print("<%30s>" %frase)
-print("<%.20s>" %frase)
-print("<%-20s>" %frase)
-print("<%-30s>" %frase)
-print("<%.30s>" %frase)
-print("<%30.20s>" %frase)
-print("<%-30.20s>" %frase)
